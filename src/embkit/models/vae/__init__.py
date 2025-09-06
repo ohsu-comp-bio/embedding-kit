87e739e845bd549dbe1ca9458ae7c3a717d59320
Kyle Ellrott <kellrott@gmail.com>
2025-09-05 23:36:37 -0700

    Changing module name to remove stutter

---
+++ src/embkit/models/vae/__init__.py
@@ -0,0 +1,6 @@
+"""
+This file initializes the Variational Autoencoder (VAE) models in the embkit library.
+It includes two classes, BaseVAE and NetVae, which are used for different types of VAEs respectively.
+"""
+from .base_vae import BaseVAE
+from .net_vae import NetVae
