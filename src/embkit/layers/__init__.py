c38427e9585ff7bd4e21c12976ea3861f03b6b07
Exitare <business.exitare@icloud.com>
2025-09-16 14:41:00 -0700

    Create tsp layer & docs (#27)

--- src/embkit/layers/__init__.py
+++ src/embkit/layers/__init__.py
@@ -11,3 +11,4 @@ Classes:
 from .masked_linear import MaskedLinear
 from .layer_info import LayerInfo, convert_activation
 from .pairwise_comparison_layer import PairwiseComparison
+from .tsp import TSPLayer
