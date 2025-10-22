1ad984750d9fe384a782d19035e8973e3b1de086
Kyle Ellrott <kellrott@gmail.com>
2025-10-22 16:49:16 -0700

    Working on commands related to debugging the alignment experiment

--- src/embkit/align.py
+++ src/embkit/align.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from scipy.stats import spearmanr
 from scipy.optimize import linear_sum_assignment
+from hopcroftkarp import HopcroftKarp
 
 def calc_rmsd(array1, array2):
     if len(array1) != len(array2):
@@ -23,9 +24,9 @@ def calc_rmsd(array1, array2):
     return rmsd
 
 
-def matrix_spearman_alignment_set(a,b,cuttoff=0.0):
+def matrix_spearman_alignment_linear(a,b,cuttoff=0.0):
     """
-    matrix_spearman_alignment_set
+    matrix_spearman_alignment_linear
     
     """
     # identify overlapping columns
@@ -44,6 +45,31 @@ def matrix_spearman_alignment_set(a,b,cuttoff=0.0):
         c = sdf.iloc[ k, v ]
         if c >= cuttoff:
             out[ a.index[v] ] = (b.index[k], c)
+    return out
+
+def matrix_spearman_alignment_hopkraft(a,b,cuttoff=0.0):
+
+    # identify overlapping columns
+    isect = a.columns.intersection( b.columns )
+    # run spearman calculation on common features
+    o = spearmanr( a[isect], b[isect], axis=1 )
+    # use the corner of the correlation matrix that is the intersection of the 2 matrices
+    a_count=a.shape[0]
+    sdf = pd.DataFrame( o.correlation[a_count:,:a_count], index=b.index, columns=a.index)
+
+    #create a bi-partite graph connecting 10 best correlated edges from each TCGA sample
+    m = {}
+    for k, row in sdf.apply(lambda x:x.nlargest(10).index, axis=0).transpose().iterrows():
+        m[k] = row.tolist()
+
+    id_map = HopcroftKarp(m).maximum_matching(keys_only=True)
+    out = {}
+    for k, v in id_map.items():
+        #c = sdf.loc[ k, v ]
+        c = sdf.loc[ v, k ]
+        if c >= cuttoff:
+            out[ k ] = (v, c)
+
     return out
 
 
--- src/embkit/preprocessing/__init__.py
+++ src/embkit/preprocessing/__init__.py
@@ -3,6 +3,5 @@ Proprocessing
 
 """
 
-from .align import calc_rmsd, matrix_spearman_alignment_set, procrustes
 from .normalize import quantile_max_norm, exp_max_norm, ExpMinMaxScaler
 from .loaders import *
