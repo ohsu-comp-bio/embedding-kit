b10b8e8139437f33148a709b3db143e0837775cc
Exitare <business.exitare@icloud.com>
2025-12-16 18:15:47 -0800

    feat: add kmeans function

--- src/embkit/utilities/pca.py
+++ src/embkit/utilities/pca.py
@@ -8,10 +8,12 @@ def run_pca(input_file: Path | str, pca_size: int, output_file: Path | str | Non
     """
     Runs PCA on the data from the input file and optionally saves the result.
 
-    :param input_file: Path to the input CSV file.
-    :param pca_size: Number of principal components to compute.
-    :param output_file: Optional path to save the PCA result as a CSV file.
-    :return: DataFrame containing the PCA-transformed data.
+    Args:
+        input_file (Path | str): Path to the input CSV file.
+        pca_size (int): Number of principal components to keep.
+        output_file (Path | str | None): Optional path to save the PCA result as a CSV file.
+    Returns:
+        pd.DataFrame: DataFrame containing the PCA-transformed data.
     """
     csvfile: LargeCsvReader = LargeCsvReader(input_file, sep="\t",
                              index_column=0, skip_header=True,
